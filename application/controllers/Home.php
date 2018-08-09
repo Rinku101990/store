8b5f0f5fe8e9994ecfd5546801b8ab2db8a8bef4
Rinku Vishwakarma <Rinku101990@gmail.com>
2018-08-09 21:37:52 +0530

    controller0908

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,6 +20,12 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		// $this->load->view("includes/header");
+		// $this->load->view('includes/main_header');
+		// $this->load->view('includes/navbar');
+		// $this->load->view('includes/side_menu');
+		// $this->load->view('includes/banners');
+		$this->load->view('home');
+		// $this->load->view('includes/footer');
 	}
 }
